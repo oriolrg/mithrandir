43aa6d0d0b2d73e73ebd941cabdba05e5616a298
Sina Samavati <sina.samv@gmail.com>
2013-07-23 04:42:22 +0430

    Add cowboy static handler

--- src/mithrandir_app.erl
+++ src/mithrandir_app.erl
@@ -9,7 +9,22 @@
 %% Application callbacks
 %% ===================================================================
 
+routes() ->
+    [
+     {'_', [
+            {"/[...]", cowboy_static, [
+                                       {directory, {priv_dir, mithrandir, [<<"www">>]}},
+                                       {file, "index.html"},
+                                       {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+                                      ]}
+           ]}
+    ].
+
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile(routes()),
+    {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
+                                                            {env, [{dispatch, Dispatch}]}
+                                                           ]),
     mithrandir_sup:start_link().
 
 stop(_State) ->
