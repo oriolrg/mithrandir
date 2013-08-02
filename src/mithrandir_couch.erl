15196518cb38e8817553878c9d0ecdc90de6e6c7
Sina Samavati <sina.samv@gmail.com>
2013-08-02 18:35:32 +0430

    Add mithrandir_couch:save_doc/2

--- src/mithrandir_couch.erl
+++ src/mithrandir_couch.erl
@@ -3,6 +3,7 @@
 -export([server/0]).
 -export([server_info/0]).
 -export([db/1]).
+-export([save_doc/2]).
 
 server() ->
     couchbeam:server_connection("localhost", 5984, "", []).
@@ -13,3 +14,7 @@ server_info() ->
 %% @doc open or create a db
 db(Name) ->
     couchbeam:open_or_create_db(server(), Name, []).
+
+%% @doc save a document
+save_doc(Db, Doc) ->
+    couchbeam:save_doc(Db, Doc).
