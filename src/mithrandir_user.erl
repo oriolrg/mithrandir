d525dc095b0ff9a7ed6d5a4d66327719db8d244f
Sina Samavati <sina.samv@gmail.com>
2013-08-06 01:39:22 +0430

    Add docstring for mithrandir_user module

--- src/mithrandir_user.erl
+++ src/mithrandir_user.erl
@@ -4,6 +4,10 @@
 -export([exists/1]).
 -export([delete/1]).
 
+%% @doc create an user
+%% Consumer must contain consumer keys
+%% @spec create(Username::binary(), Consumer::list(), AccessToken::binary(),
+%%              AccessTokenSecret::binary())
 create(Username, Consumer, AccessToken, AccessTokenSecret) ->
     Postfix = list_to_binary(string:to_lower(binary_to_list(AccessTokenSecret))),
     DbName = <<Username/binary, <<"-">>/binary, Postfix/binary>>,
@@ -18,8 +22,10 @@ create(Username, Consumer, AccessToken, AccessTokenSecret) ->
     {ok, _} = mithrandir_couch:save_doc(Db, Doc),
     {ok, DbName}.
 
+%% @doc check if an user exists
 exists(UserId) ->
     mithrandir_couch:db_exists(UserId).
 
+%% @doc delete an user
 delete(UserId) ->
     mithrandir_couch:delete_db(UserId).
