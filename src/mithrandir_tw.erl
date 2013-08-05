dad6c7366c6e56eed2af8ee8e168e9af5c35c8fc
Sina Samavati <sina.samv@gmail.com>
2013-08-06 01:23:59 +0430

    Add mithrandir_tw:update/1 to store each tweet into couchdb

--- src/mithrandir_tw.erl
+++ src/mithrandir_tw.erl
@@ -1,5 +1,7 @@
 -module(mithrandir_tw).
+
 -export([fetch/1]).
+-export([update/1]).
 
 fetch(UserId) ->
     {ok, Doc} = mithrandir_couch:get_doc(UserId, <<"user">>),
@@ -9,3 +11,11 @@ fetch(UserId) ->
     AccessTokenSecret = binary_to_list(couchbeam_doc:get_value(<<"accesstokensecret">>, Doc)),
     URL = "https://api.twitter.com/1.1/statuses/home_timeline.json",
     oauth:get(URL, [], Consumer, AccessToken, AccessTokenSecret).
+
+update(UserId) ->
+    {ok, {_, _, Res}} = fetch(UserId),
+    Docs = couchbeam_ejson:decode(Res),
+    lists:foreach(fun(Doc) ->
+                          mithrandir_couch:save_doc(UserId, Doc)
+                  end,
+                  Docs).
