2aaffc05805ec3413aa86c52af19638c53bdddea
Sina Samavati <sina.samv@gmail.com>
2013-08-06 02:10:42 +0430

    Add docstring for mithrandir_tw

--- src/mithrandir_tw.erl
+++ src/mithrandir_tw.erl
@@ -3,6 +3,8 @@
 -export([fetch/1]).
 -export([update/1]).
 
+%% @doc fetch an user's timeline
+%% @spec fetch(UserId::bitstring()) -> {ok, tuple()}
 fetch(UserId) ->
     {ok, Doc} = mithrandir_couch:get_doc(UserId, <<"user">>),
     _Consumer = couchbeam_doc:get_value(<<"consumer">>, Doc),
@@ -12,6 +14,8 @@ fetch(UserId) ->
     URL = "https://api.twitter.com/1.1/statuses/home_timeline.json",
     oauth:get(URL, [], Consumer, AccessToken, AccessTokenSecret).
 
+%% @doc fetch and store to db
+%% @spec update(UserId::bitstring()) -> ok
 update(UserId) ->
     {ok, {_, _, Res}} = fetch(UserId),
     Docs = couchbeam_ejson:decode(Res),
