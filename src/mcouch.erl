eebdb5a2188647aa25f0e1f3c97190a9f7f0845a
Sina Samavati <sina.samv@gmail.com>
2013-10-08 00:48:19 +0330

    Rename server/0 to server_connection/0 & add a bunch of specs

--- src/mcouch.erl
+++ src/mcouch.erl
@@ -1,6 +1,6 @@
 -module(mcouch).
 
--export([server/0]).
+-export([server_connection/0]).
 -export([server_info/0]).
 -export([db/1]).
 -export([save_doc/2]).
@@ -8,37 +8,43 @@
 -export([db_exists/1]).
 -export([delete_db/1]).
 
-server() ->
+-include_lib("couchbeam/include/couchbeam.hrl").
+
+
+-spec server_connection() -> server().
+server_connection() ->
     couchbeam:server_connection("localhost", 5984, "", []).
 
 server_info() ->
-    couchbeam:server_info(server()).
+    couchbeam:server_info(server_connection()).
 
 %% @doc open or create a db
-%% @spec db(DbName::bitstring()) -> {ok, tuple()} | {error, tuple()}
-db(DbName) ->
-    couchbeam:open_or_create_db(server(), DbName, []).
+-spec db(db_name()) -> {ok, db()} | {error, any()}.
+db(Name) ->
+    couchbeam:open_or_create_db(server_connection(), Name, []).
 
 %% @doc save a document
+-spec save_doc(db() | db_name(), ejson()) -> {ok, ejson()} | {error, any()}.
 save_doc(Db, Doc) when is_tuple(Db) ->
     couchbeam:save_doc(Db, Doc);
 save_doc(DbName, Doc) ->
-    {ok, Db} = couchbeam:open_db(server(), DbName),
+    {ok, Db} = couchbeam:open_db(server_connection(), DbName),
     couchbeam:save_doc(Db, Doc).
 
 %% @doc get a document by id
+-spec get_doc(db() | db_name(), docid()) -> {ok, ejson()} | {error, any()}.
 get_doc(Db, Id) when is_tuple(Db) ->
     couchbeam:open_doc(Db, Id);
 get_doc(DbName, Id) ->
-    {ok, Db} = couchbeam:open_db(server(), DbName),
+    {ok, Db} = couchbeam:open_db(server_connection(), DbName),
     couchbeam:open_doc(Db, Id).
 
 %% @doc check if a db exists
-%% @spec db_exists(DbName) -> boolean()
+-spec db_exists(db_name()) -> boolean().
 db_exists(DbName) ->
-    couchbeam:db_exists(server(), DbName).
+    couchbeam:db_exists(server_connection(), DbName).
 
 %% @doc delete a db
-%% @spec delete_db(DbName::bitstring()) -> {ok, tuple()} | {error, tuple()}
+-spec delete_db(db_name()) -> {ok, iolist()} | {error, any()}.
 delete_db(DbName) ->
-    couchbeam:delete_db(server(), DbName).
+    couchbeam:delete_db(server_connection(), DbName).
